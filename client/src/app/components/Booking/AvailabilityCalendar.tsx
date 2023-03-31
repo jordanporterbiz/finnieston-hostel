7dd92045ae3afc1b74ec58594a78fcdfe3f05912
Jordan <jordanporter94@outlook.com>
2023-03-31 18:09:29 +0100

    Add initial components needed for booking flow

---
+++ client/src/app/components/Booking/AvailabilityCalendar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AvailabilityCalendar() {
+	return <div>AvailabilityCalendar</div>;
+}
+
+export default AvailabilityCalendar;
---
+++ client/src/app/components/Booking/Search.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+// Material UI form which takes in
+// Check in date, check out date, number of guests
+// And Searches for available rooms
+const Search = () => {
+	return;
+	<div></div>;
+};
+
+export default Search;
